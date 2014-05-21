9f7e2470e8511dfb9504b0cc06211397f151718d
Stina Qvarnström <stinaqv@gmail.com>
2014-05-21 22:37:26 +0200

    Trying to show time diff

--- app/scripts/controllers/timeLeftCtrl.js
+++ app/scripts/controllers/timeLeftCtrl.js
@@ -3,11 +3,24 @@
 angular.module('countingDown.controllers')
 .controller('TimeLeftCtrl', function ($scope, $timeout, $moment) {
 
-  var testTime = 1400712273;
+  var testTime = 1400701273;
+
+  $scope.years = 0;
+  $scope.days = 0;
+  $scope.months = 0;
+  $scope.days = 0;
+  $scope.hours = 0;
+  $scope.minutes = 0;
+  $scope.seconds = 0;
 
   var tickDown = function() {
-    var now = moment();
-    $scope.date = now.diff($moment.unix(testTime), 'seconds');
+    var now = $moment();
+    var endTime = $moment.unix(testTime);
+
+    $scope.days = endTime.diff(now, 'days');
+    $scope.hours = endTime.diff(now, 'hours');
+    $scope.minutes = endTime.diff(now, 'minutes');
+    $scope.seconds = endTime.diff(now, 'seconds');
     $timeout(tickDown, 1000);
   };
 
