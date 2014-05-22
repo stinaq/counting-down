9b84eeb55ae2e919ad25011b7e000c056b01e3fe
Stina Qvarnström <stinaqv@gmail.com>
2014-05-22 23:40:04 +0200

    Showing out times

--- app/scripts/controllers/timeLeftCtrl.js
+++ app/scripts/controllers/timeLeftCtrl.js
@@ -3,11 +3,9 @@
 angular.module('countingDown.controllers')
 .controller('TimeLeftCtrl', function ($scope, $timeout, $moment) {
 
-  var testTime = 1400701273;
+  var testTime = 1411088460;
 
   $scope.years = 0;
-  $scope.days = 0;
-  $scope.months = 0;
   $scope.days = 0;
   $scope.hours = 0;
   $scope.minutes = 0;
@@ -54,11 +52,16 @@ angular.module('countingDown.controllers')
   var tickDown = function() {
     var now = $moment();
     var endTime = $moment.unix(testTime);
+    var sumSeconds = endTime.diff(now, 'seconds');
 
-    $scope.days = endTime.diff(now, 'days');
-    $scope.hours = endTime.diff(now, 'hours');
-    $scope.minutes = endTime.diff(now, 'minutes');
-    $scope.seconds = endTime.diff(now, 'seconds');
+    var parsedTime = parseSecondsToPrettyTime(sumSeconds);
+
+    $scope.years = parsedTime.years;
+    $scope.days = parsedTime.days;
+    $scope.hours = parsedTime.hours;
+    $scope.minutes = parsedTime.minutes;
+    $scope.seconds = parsedTime.seconds;
+
     $timeout(tickDown, 1000);
   };
 
