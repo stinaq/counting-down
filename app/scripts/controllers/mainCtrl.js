02b8ede79ffcf1766a5e6cafd5c4720b50063d4e
Stina Qvarnström <stinaqv@gmail.com>
2014-05-25 23:32:42 +0200

    Setting up scope variables

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -3,8 +3,13 @@
 angular.module('countingDown.controllers')
 .controller('MainCtrl', function ($scope, $timeout, $moment, $location) {
 
-  $scope.timeDate = '';
-  $scope.title = '';
+  $scope.timeDateInput = '';
+  $scope.title = 'hgf';
+  $scope.years = 0;
+  $scope.days = 0;
+  $scope.hours = 0;
+  $scope.minutes = 0;
+  $scope.seconds = 0;
 
   $scope.hasValidDate = false;
 
