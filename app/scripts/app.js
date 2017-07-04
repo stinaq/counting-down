5221738b36abccd7834878b7881ff61822a1495d
Stina Qvarnström <stinaqv@gmail.com>
2017-07-04 21:47:40 +0200

    Main app file

---
+++ app/scripts/app.js
@@ -0,0 +1,44 @@
+(function() {
+
+  /*
+
+  * Read time from query parameter
+  * Read title from parameter
+  * Set time in url
+  * Ska man göra nån slags pub/sub? Publicera till en state? ha en global app och göra ping till
+    den när något hänt. Och varje gång man pushat ett state så är det en "viewer" som bestämmer
+    vad som ska visas ut. Ska man göra två olika delar i htmlen? och bara sätta
+    visa/inte visa på dem?
+    Positivt med det är att man inte behöver rendera om markupen, den är där redan
+
+    view.ping
+
+    Vad behöver hända?
+    Registrera vyer, sen slå av och på dem vid olika events
+
+  */
+
+
+
+
+})();
+
+let AppState = (() => {
+  let allThings = {};
+
+  // sätt upp en watcher på locationChange
+
+  const ViewDecider = (name) => {
+    if (name) {
+      allThings[name].condition();
+    }
+  };
+
+  return {
+    RegisterThing: (name, element, condition) => {
+      console.log(`Registering ${name}`);
+      allThings[name] = { element, condition };
+      ViewDecider(name);
+    }
+  };
+})();
