d7191f86531c1849fddac6109a3ddd4d5ab3c93f
Stina Qvarnström <stinaqv@gmail.com>
2017-07-04 21:48:00 +0200

    Clock file

---
+++ app/scripts/Clock.js
@@ -0,0 +1,11 @@
+let Clock = (function(argument) {
+
+
+  return {
+    condition: () => {
+      console.log('callback');
+
+      // Visas om det finns något efter query string? Om det finns tid
+    }
+  }
+})();
