6c402ddbd07a32532c18cdae689e1de7797a85bb
Stina Qvarnström <stina@stinaq.se>
2017-07-28 18:18:48 +0200

    Ticking the clock, and showing and hiding

--- app/js/Clock.js
+++ app/js/Clock.js
@@ -1,11 +1,48 @@
-let Clock = (function(argument) {
+const Clock = (function(argument) {
+  const id = 'TICKING_CLOCK';
+  let tickerInterval;
 
+  const startTick = (mainElement) => {
+    const clock = mainElement.querySelector('#clock');
+    const goal = window.location.hash.substr(1);
+
+    const tick = () => {
+      const secondsLeft = goal - Math.floor(Date.now() / 1000);
+      if (secondsLeft < 0) {
+        clock.textContent = 'Has passed';
+      } else {
+        clock.textContent = secondsLeft;
+      }
+    };
+
+    tick();
+    clearInterval(tickerInterval);
+    tickerInterval = setInterval(tick, 1000);
+  };
+
+  const stopTick = () => {
+    clearInterval(tickerInterval);
+  };
 
   return {
+    id: id,
     condition: () => {
-      console.log('callback');
+      const hash = window.location.hash.substr(1);
+      const isNumeric = !isNaN(hash);
+      const isSomething = hash !== '';
 
-      // Visas om det finns något efter query string? Om det finns tid
+      /* Show if everything after the hash is a number,
+         meaning it could be timestamp to show */
+      return isNumeric && isSomething;
+    },
+    show: () => {
+      const clockView = document.getElementById(id);
+      startTick(clockView);
+      clockView.style.display = 'inherit';
+    },
+    hide: () => {
+      const clockView = document.getElementById(id);
+      clockView.style.display = 'none';
     }
   }
 })();
