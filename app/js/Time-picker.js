9d20317875c23bacd963ce0c5dcc984d95e786fe
Stina Qvarnström <stina@stinaq.se>
2017-08-27 21:33:43 +0200

    Moving the start button to a named function

--- app/js/Time-picker.js
+++ app/js/Time-picker.js
@@ -1,5 +1,10 @@
 const TimePicker = (function(argument) {
   const id = 'TIME_PICKER';
+  const startCountdown = () => {
+    const date = document.querySelector('#TIME_PICKER_data-input').value;
+    const timeStamp = Math.floor(new Date(date) / 1000);
+    window.location.hash = timeStamp;
+  };
 
   return {
     id: id,
@@ -13,11 +18,7 @@ const TimePicker = (function(argument) {
       timePickerView.style.display = 'inherit';
 
       const startButton = document.querySelector('#TIME_PICKER_start-button');
-      startButton.addEventListener('click', () => {
-        const date = document.querySelector('#TIME_PICKER_data-input').value;
-        const timeStamp = Math.floor(new Date(date) / 1000);
-        window.location.hash = timeStamp;
-      });
+      startButton.addEventListener('click', startCountdown);
     },
     hide: () => {
       const timePickerView = document.getElementById(id);
