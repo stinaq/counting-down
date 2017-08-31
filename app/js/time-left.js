f446f9b47c4784da5514d5b774599dec709946db
Stina Qvarnström <stina@stinaq.se>
2017-08-31 21:32:32 +0200

    Utility for calculating a pretty way of showing time left

---
+++ app/js/time-left.js
@@ -0,0 +1,11 @@
+const timeLeft = (function(argument) {
+  const pretty = (startSeconds, goalSeconds) => {
+    return {hours: 0, minutes:0};
+  };
+
+  return {
+    pretty
+  }
+})();
+
+module.exports = timeLeft;
