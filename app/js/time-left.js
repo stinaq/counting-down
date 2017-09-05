2a6c890fbe4703bdfd1449c046616229167d0e0c
Stina Qvarnström <stina@stinaq.se>
2017-09-05 23:29:44 +0200

    Tests for seconds and minutes

--- app/js/time-left.js
+++ app/js/time-left.js
@@ -1,15 +1,42 @@
 const timeLeft = (function(argument) {
 
-  const pretty = (startSeconds, goalSeconds) => {
+  const pretty = (startTimeStamp, goalTimeStamp) => {
+    const startDate = new Date(startTimeStamp * 1000);
+    const goalDate = new Date(goalTimeStamp * 1000);
+
     let secondsDiff,
         minutesDiff,
         hoursDiff,
+        daysDiff,
         monthsDiff,
         yearsDiff;
 
-    secondsDiff = dateFns.getSeconds(goalSeconds);
+    yearsDiff = dateFns.getYear(goalDate) - dateFns.getYear(startDate);
+    monthsDiff = dateFns.getMonth(goalDate) - dateFns.getMonth(startDate);
+    daysDiff = dateFns.getDate(goalDate) - dateFns.getDate(startDate);
+    hoursDiff = dateFns.getHours(goalDate) - dateFns.getHours(startDate);
+    minutesDiff = dateFns.getMinutes(goalDate) - dateFns.getMinutes(startDate);
+    secondsDiff = dateFns.getSeconds(goalDate) - dateFns.getSeconds(startDate);
 
-    return {seconds:5};
+    if (secondsDiff < 0) {
+      secondsDiff = secondsDiff + 60;
+      minutesDiff = minutesDiff - 1;
+    }
+
+    if (minutesDiff < 0) {
+      minutesDiff = minutesDiff + 60;
+      hoursDiff = hoursDiff - 1;
+    }
+
+// debugger
+    return {
+      years: yearsDiff,
+      months: monthsDiff,
+      days: daysDiff,
+      hours: hoursDiff,
+      minutes: minutesDiff,
+      seconds: secondsDiff
+    };
   };
 
   return {
--- jasmine/spec/time-left.spec.js
+++ jasmine/spec/time-left.spec.js
@@ -1,7 +1,132 @@
 describe("Time left", function() {
+  describe('when there is only seconds between the dates', function() {
+    describe('and the seconds are inside one minute', () => {
+      var startDate = 1504549527;
+      var goalDate = 1504549532;
+      var result;
 
-  it('should have seconds', function() {
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
 
-    expect(timeLeft.pretty(1, 4)).toEqual({seconds:5});
+      it('should have 0 years, 0 months, 0 days, 0 hours, 0 minutes, 20 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 5
+        });
+      });
+
+    });
+    describe('when the seconds overlap into a different minute', function() {
+      // Tuesday, 5 September 2017 21:46:50 GMT+02:00
+      var startDate = 1504640810;
+      // Tuesday, 5 September 2017 21:47:10 GMT+02:00
+      var goalDate = 1504640830;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 0 days, 0 hours, 0 minutes, 20 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 20
+        });
+      });
+    });
+
   });
+  describe('when there is minutes and seconds between', function() {
+    describe('and the minutes and seconds are both inside one hour and one minute', function() {
+      // Tuesday, 5 September 2017 22:35:30 GMT+02:00
+      var startDate = 1504643730;
+      // Tuesday, 5 September 2017 22:47:53 GMT+02:00
+      var goalDate = 1504644473;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+      it('should have 0 years, 0 months, 0 days, 0 hours, 12 minutes, 23 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 12,
+          seconds: 23
+        });
+      });
+    });
+    fdescribe('and the minutes and seconds are both overlapping into a different minute and hour', function() {
+      // Tuesday, 5 September 2017 11:38:11 GMT+02:00
+      var startDate = 1504604291;
+      // Tuesday, 5 September 2017 12:11:03 GMT+02:00
+      var goalDate = 1504606263;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+      it('should have 0 years, 0 months, 0 days, 0 hours, 33 minutes, 52 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 33,
+          seconds: 52
+        });
+      });
+    });
+
+    var startDate = 1504552410;
+    var goalDate = 1504554462;
+    var result;
+
+    beforeEach(() => {
+      result = timeLeft.pretty(startDate, goalDate);
+    });
+
+    it('should have 0 years, 0 months, 0 days, 0 hours, 34 minutes, 12 seconds', function() {
+      expect(result).toEqual({
+        years: 0,
+        months: 0,
+        days: 0,
+        hours: 0,
+        minutes: 34,
+        seconds: 12
+      });
+    });
+  });
+  xdescribe('when there is 16 hours, 45 minutes and 25 seconds between the dates', function() {
+    var startDate = 1504553162;
+    var goalDate = 1504595487;
+    var result;
+
+    beforeEach(() => {
+      result = timeLeft.pretty(startDate, goalDate);
+    });
+
+    it('should have 0 years, 0 months, 0 days, 16 hours, 45 minutes, 25 seconds', function() {
+      expect(result).toEqual({
+        years: 0,
+        months: 0,
+        days: 0,
+        hours: 16,
+        minutes: 45,
+        seconds: 25
+      });
+    });
+  });
+
 });
