5f9ec5623df6b0c284922f9f1af51be2f4e1450e
Stina Qvarnström <stina@stinaq.se>
2017-09-13 23:12:30 +0200

    Calculating the number of days between dates

--- app/js/time-left.js
+++ app/js/time-left.js
@@ -13,10 +13,13 @@ const timeLeft = (function(argument) {
 
     yearsDiff = dateFns.getYear(goalDate) - dateFns.getYear(startDate);
     monthsDiff = dateFns.getMonth(goalDate) - dateFns.getMonth(startDate);
+    // daysDiff = dateFns.differenceInCalendarDays(goalDate, startDate)
     daysDiff = dateFns.getDate(goalDate) - dateFns.getDate(startDate);
     hoursDiff = dateFns.getHours(goalDate) - dateFns.getHours(startDate);
     minutesDiff = dateFns.getMinutes(goalDate) - dateFns.getMinutes(startDate);
     secondsDiff = dateFns.getSeconds(goalDate) - dateFns.getSeconds(startDate);
+
+// debugger
 
     if (secondsDiff < 0) {
       secondsDiff = secondsDiff + 60;
@@ -25,10 +28,26 @@ const timeLeft = (function(argument) {
 
     if (minutesDiff < 0) {
       minutesDiff = minutesDiff + 60;
-      hoursDiff = hoursDiff - 1;
+      if (hoursDiff !== 0) {
+        hoursDiff = hoursDiff - 1;
+      }
     }
 
-// debugger
+    if (hoursDiff < 0) {
+      hoursDiff = hoursDiff + 24;
+      daysDiff = daysDiff - 1;
+    }
+
+    if (daysDiff < 0) {
+      // there is no fixed amount of days for each month, therfor
+      // the days to add to the negative amount must be calculated using
+      // the last month before the goal month
+      const lastMonthBeforeGoal = dateFns.subMonths(goalDate, 1);
+      const numberOfDaysInMonth = dateFns.getDaysInMonth(lastMonthBeforeGoal);
+      daysDiff = daysDiff + numberOfDaysInMonth;
+      monthsDiff = monthsDiff - 1;
+    }
+
     return {
       years: yearsDiff,
       months: monthsDiff,
--- jasmine/spec/time-left.spec.js
+++ jasmine/spec/time-left.spec.js
@@ -9,7 +9,7 @@ describe("Time left", function() {
         result = timeLeft.pretty(startDate, goalDate);
       });
 
-      it('should have 0 years, 0 months, 0 days, 0 hours, 0 minutes, 20 seconds', function() {
+      it('should have 0 years, 0 months, 0 days, 0 hours, 0 minutes, 5 seconds', function() {
         expect(result).toEqual({
           years: 0,
           months: 0,
@@ -67,7 +67,7 @@ describe("Time left", function() {
         });
       });
     });
-    fdescribe('and the minutes and seconds are both overlapping into a different minute and hour', function() {
+    describe('and the minutes and seconds are both overlapping into a different minute and hour', function() {
       // Tuesday, 5 September 2017 11:38:11 GMT+02:00
       var startDate = 1504604291;
       // Tuesday, 5 September 2017 12:11:03 GMT+02:00
@@ -77,56 +77,198 @@ describe("Time left", function() {
       beforeEach(() => {
         result = timeLeft.pretty(startDate, goalDate);
       });
-      it('should have 0 years, 0 months, 0 days, 0 hours, 33 minutes, 52 seconds', function() {
+      it('should have 0 years, 0 months, 0 days, 0 hours, 32 minutes, 52 seconds', function() {
         expect(result).toEqual({
           years: 0,
           months: 0,
           days: 0,
           hours: 0,
-          minutes: 33,
+          minutes: 32,
           seconds: 52
         });
       });
     });
+    describe('the minutes are overlapping into another hour, but the seconds are not', function() {
+      // Wednesday, 6 September 2017 20:52:57 GMT+02:00
+      var startDate = 1504723977;
+      // Wednesday, 6 September 2017 21:03:59 GMT+02:00
+      var goalDate = 1504721039;
+      var result;
 
-    var startDate = 1504552410;
-    var goalDate = 1504554462;
-    var result;
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+      it('should have 0 years, 0 months, 0 days, 0 hours, 11 minutes, 2 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 11,
+          seconds: 2
+        });
+      });
+    });
+    describe('the seconds are overlapping into another minute, but the minutes are not', function() {
+      // Wednesday, 6 September 2017 21:46:23 GMT+02:00
+      var startDate = 1504727183;
+      // Wednesday, 6 September 2017 21:56:09 GMT+02:00
+      var goalDate = 1504727769;
+      var result;
 
-    beforeEach(() => {
-      result = timeLeft.pretty(startDate, goalDate);
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+      it('should have 0 years, 0 months, 0 days, 0 hours, 9 minutes, 46 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 9,
+          seconds: 46
+        });
+      });
     });
 
-    it('should have 0 years, 0 months, 0 days, 0 hours, 34 minutes, 12 seconds', function() {
-      expect(result).toEqual({
-        years: 0,
-        months: 0,
-        days: 0,
-        hours: 0,
-        minutes: 34,
-        seconds: 12
+  });
+
+  describe('when there is hours, minutes and seconds between the dates', function() {
+    describe('when seconds, minutes and hours don\'t overlap into other seconds, minutes and hours', function() {
+      // Wednesday, 6 September 2017 17:13:41 GMT+02:00
+      var startDate = 1504710821;
+      // Wednesday, 6 September 2017 20:35:49 GMT+02:00
+      var goalDate = 1504722949;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 0 days, 3 hours, 22 minutes, 8 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 3,
+          minutes: 22,
+          seconds: 8
+        });
+      });
+    });
+    describe('when the hours overlap into another day, but not the other parts', function() {
+      // Wednesday, 6 September 2017 22:24:09 GMT+02:00
+      var startDate = 1504729449;
+      // Thursday, 7 September 2017 09:24:09 GMT+02:00
+      var goalDate = 1504769049;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 0 days, 11 hours, 0 minutes, 0 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 11,
+          minutes: 0,
+          seconds: 0
+        });
+      });
+    });
+    describe('when the hours and minutes overlap into another day and hours, but not the seconds', function() {
+      // Thursday, 7 September 2017 21:14:16 GMT+02:00
+      var startDate = 1504811656;
+      // Friday, 8 September 2017 05:07:19 GMT+02:00
+      var goalDate = 1504840039;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 0 days, 7 hours, 53 minutes, 3 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 7,
+          minutes: 53,
+          seconds: 3
+        });
+      });
+    });
+    describe('when all the parts overlap', function() {
+      // Sunday, 10 September 2017 11:48:31 GMT+02:00
+      var startDate = 1505036911;
+      // Monday, 11 September 2017 08:22:12 GMT+02:00
+      var goalDate = 1505110932;
+      var result;
+
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 0 days, 20 hours, 33 minutes, 41 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 20,
+          minutes: 33,
+          seconds: 41
+        });
       });
     });
   });
-  xdescribe('when there is 16 hours, 45 minutes and 25 seconds between the dates', function() {
-    var startDate = 1504553162;
-    var goalDate = 1504595487;
-    var result;
+  describe('When there are days, hours, minutes and seconds between the dates', function() {
+    describe('and none of the parts are overlapping into the next day/hour and so on', function() {
+      // Tuesday, 12 September 2017 19:57:36 GMT+02:00
+      var startDate = 1505239056;
+      // Wednesday, 13 September 2017 23:58:45 GMT+02:00
+      var goalDate = 1505339925;
+      var result;
 
-    beforeEach(() => {
-      result = timeLeft.pretty(startDate, goalDate);
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 1 days, 4 hours, 1 minutes, 9 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 1,
+          hours: 4,
+          minutes: 1,
+          seconds: 9
+        });
+      });
     });
+    fdescribe('when the days are overlapping into another month', function() {
+      // Tuesday, 12 September 2017 19:57:36 GMT+02:00
+      var startDate = 1505239056;
+      // Monday, 9 October 2017 23:58:45 GMT+02:00
+      var goalDate = 1507586325;
+      var result;
 
-    it('should have 0 years, 0 months, 0 days, 16 hours, 45 minutes, 25 seconds', function() {
-      expect(result).toEqual({
-        years: 0,
-        months: 0,
-        days: 0,
-        hours: 16,
-        minutes: 45,
-        seconds: 25
+      beforeEach(() => {
+        result = timeLeft.pretty(startDate, goalDate);
+      });
+
+      it('should have 0 years, 0 months, 27 days, 4 hours, 1 minutes, 9 seconds', function() {
+        expect(result).toEqual({
+          years: 0,
+          months: 0,
+          days: 27,
+          hours: 4,
+          minutes: 1,
+          seconds: 9
+        });
       });
     });
   });
+
 
 });
